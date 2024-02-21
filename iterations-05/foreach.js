ca35dd63904860ed2fb25393689e0131112f9c60
uday kumar <uday33379@gmail.com>
2024-02-21 13:35:05 +0530

    foreach loop

---
+++ iterations-05/foreach.js
@@ -0,0 +1,40 @@
+const coding =["js","css","cpp","html","python"]
+
+// coding.forEach(function(item){
+//  console.log(item)
+// })
+//arrow function
+// coding.forEach((element)=>{
+//     console.log(element)
+// })
+
+
+// function printme(item){
+//     console.log(item)
+// }
+// coding.forEach(printme)
+
+
+// coding.forEach((item,index,arr)=>{
+// console.log(item,index,arr);
+// })
+
+
+const mycoding=[
+    {
+        languageName:'javascript',
+        languagefile:'js'
+    },
+    {
+        languageName:'python',
+        languagefile:'py'
+    },
+    {
+        languageName:'java',
+        languagefile:'java'
+    }
+]
+mycoding.forEach((item)=>{
+    console.log(item.languageName)
+})
+
