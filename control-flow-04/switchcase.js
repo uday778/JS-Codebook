046522421724a2008f6c033f8a52f5628d7abd7c
uday kumar <uday33379@gmail.com>
2024-02-21 11:33:15 +0530

    switch case

---
+++ control-flow-04/switchcase.js
@@ -0,0 +1,19 @@
+// node switchcase.js
+
+// switch case syntax
+const month=0
+switch (month) {
+    case 1:
+        console.log("jan")
+        break;
+    case 2:
+        console.log("feb")
+        break;
+    case 3:
+        console.log("march")
+        break;
+
+    default:
+        console.log("default case matched")
+        break;
+}
